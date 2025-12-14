243a94ee03e756e5c6ab45d1205077f363a9ebbc
wjx_your_dad <guess@what.com>
2025-12-15 00:04:55 +0800

    int10~0递减_v2.cpp

---
+++ int10~0递减_v2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int val = 10;
+    while (val >= 0)
+    {
+        cout << val << endl;
+        val--;
+    }
+    // 嘎嘎嘎嘎嘎嘎嘎嘎嘎嘎嘎
+
+    return 0;
+}
