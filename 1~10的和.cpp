0ecab54597c5189c8b2ba261d76299226e0f3fec
wjx_your_dad <guess@what.com>
2025-12-14 23:54:10 +0800

    int10~0递减.cpp

--- 1~10的和.cpp
+++ 1~10的和.cpp
@@ -11,4 +11,5 @@ int main()
     }
 
     cout << sum;
+    return 0;
 }
---
+++ int10~0递减.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int val = 10;
+    cout << 10 << endl;
+    while (val > 0)
+    {
+        val--;
+        cout << val << endl;
+    }
+
+    return 0;
+}
